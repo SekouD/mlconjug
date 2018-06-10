26810af5fe1aa4dcf7c710596f6615fe572326e4
SekouD <sekou.diao@gmail.com>
2018-06-10 10:04:54 +0200

    Appveyor configuration.

--- mlconjug/PyVerbiste.py
+++ mlconjug/PyVerbiste.py
@@ -36,7 +36,7 @@ class Verbiste:
 
         """
         verbs_dic = {}
-        with codecs.open(verbs_path, "r") as file:
+        with codecs.open(verbs_path, "r", encoding='utf-8') as file:
             xml = ET.parse(file)
             for verb in xml.findall("v"):
                 verb_name = verb.find("i").text
@@ -52,7 +52,7 @@ class Verbiste:
 
         """
         conjugations_dic = {}
-        with codecs.open(conjugations_path, "r") as file:
+        with codecs.open(conjugations_path, "r", encoding='utf-8') as file:
             xml = ET.parse(file)
             for template in xml.findall("template"):
                 template_name = template.get("name")
