daa96f8dd6cf660d1c21b6fd1230d67cceebe3ea
SekouD <sekou.diao@gmail.com>
2018-06-10 14:50:29 +0200

    Refactored mlconjug and updated comments for documentation purpose.

--- mlconjug/mlconjug.py
+++ mlconjug/mlconjug.py
@@ -3,7 +3,7 @@
 """Main module."""
 
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.svm import LinearSVC, SVC
+from sklearn.svm import LinearSVC
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 
@@ -13,15 +13,29 @@ from collections import defaultdict, OrderedDict
 
 
 class EndingCountVectorizer(CountVectorizer):
-    """Custom Vectorizer optimized for extracting verbs features"""
-    def _char_ngrams(self, text_document):
-        """Tokenize text_document into a sequence of ending character n-grams"""
-        text_document = self._white_spaces.sub(" ", text_document)
-        text_len = len(text_document)
+    """
+    Custom Vectorizer optimized for extracting verbs features.
+    The Vectorizer subclasses sklearn.feature_extraction.text.CountVectorizer
+    As in Romance languages verbs are inflected by adding a morphological suffix,
+    the vectorizer extracts verb endings and produces a vector representation of the verb with binary features.
+    The features are the verb ending ngrams. (ngram_range is set at class initialization).
+
+    """
+    def _char_ngrams(self, verb):
+        """
+        Parses a verb and returns the ending ngrams.
+
+        :param verb: string.
+            Verb to vectorize.
+        :return: list.
+            Final ngrams of the verb.
+        """
+        verb = self._white_spaces.sub(" ", verb)
+        verb_len = len(verb)
         ngrams = []
         min_n, max_n = self.ngram_range
-        for n in range(min_n, min(max_n + 1, text_len + 1)):
-            ngram = text_document[-n:]
+        for n in range(min_n, min(max_n + 1, verb_len + 1)):
+            ngram = verb[-n:]
             ngrams.append(ngram)
         return ngrams
 
@@ -29,10 +43,12 @@ class EndingCountVectorizer(CountVectorizer):
 
 class DataSet:
     """
+    This class holds and manages the data set.
+    Defines helper functions for managing Machine Learning Tasks.
+
 
     :param VerbisteObj:
 
-    :return:
     """
     def __init__(self, VerbisteObj):
         self.verbiste = VerbisteObj
@@ -48,6 +64,7 @@ class DataSet:
 
     def construct_dict_conjug(self):
         """
+        Populates the dictionary containing the conjugation templates.
 
         :return:
         """
@@ -62,9 +79,12 @@ class DataSet:
 
     def split_data(self, threshold=8, proportion=0.5):
         """
+        Splits the data into a training and a testing set.
 
-        :param threshold:
-        :param proportion:
+        :param threshold: int.
+            Minimum size of conjugation class to be split.
+        :param proportion: float.
+            Proportion of samples in the training set. Must be between 0 and 1.
         :return:
         """
         if proportion <= 0 or proportion >= 1:
@@ -95,27 +115,49 @@ class DataSet:
 
 class Model(object):
     """
+    This class manages the scikit-learn model.
+    The Pipeline includes a feature vectorizer, a feature selector and a classifier.
+    If any of the vectorizer, feature selector or classifier is not supplied at instance declaration,
+    the __init__ method will provide good default value that gets more than 98% prediction accuracy.
 
-    :param vectorizer:
-    :param feature_selector:
-    :param classifier:
+    :param vectorizer: scikit-learn Vectorizer.
+    :param feature_selector: scikit-learn Classifier with a fit_transform() method
+    :param classifier: scikit-learn Classifier with a predict() method
     """
-    def __init__(self, vectorizer, feature_selector, classifier):
+    def __init__(self, vectorizer=None, feature_selector=None, classifier=None):
+        if not vectorizer:
+            vectorizer = EndingCountVectorizer(analyzer="char", binary=True, ngram_range=(2, 7))
+        if not feature_selector:
+            feature_selector = LinearSVC(penalty='l1', max_iter=3000, dual=False, verbose=2)
+        if not classifier:
+            classifier = SGDClassifier(loss='log', penalty='elasticnet', alpha=1e-5, random_state=42)
         self.model = Pipeline([('vectorizer', vectorizer),
                                ('feature_selector', feature_selector),
                                ('classifier', classifier)])
 
     def train(self, samples, labels):
         """
+        Trains the model on the supplied samples and labels.
 
-        :param samples:
-        :param labels:
+        :param samples: list.
+            List of verbs.
+        :param labels: list.
+            List of verb templates.
         """
         self.model = self.model.fit(samples, labels)
 
     def predict(self, verbs):
         """
+        Predicts the conjugation class of the provided list of verbs.
 
-        :param verbs:
+        :param verbs: list.
+            List of verbs.
+        :return: list.
+            List of predicted conjugation groups.
         """
         prediction = self.model.predict(verbs)
+        return prediction
+
+
+if __name__ == "__main__":
+    pass
