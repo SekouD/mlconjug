957dc790f360bb400b86e37fae14db8dafbc28aa
SekouD <sekou.diao@gmail.com>
2018-06-12 13:50:09 +0200

    removed unused arguments for now.

--- mlconjug/cli.py
+++ mlconjug/cli.py
@@ -13,14 +13,6 @@ from pprint import pprint
               default='fr',
               help='The language for the conjugation model.',
               type=click.STRING)
-@click.option('-m', '--mood',
-              default=None,
-              help='The mood to conjugate. By default displays all moods.',
-              type=click.STRING)
-@click.option('-t', '--tense',
-              default=None,
-              help='The tense to conjugate. By default displays all tense.',
-              type=click.STRING)
 def main(verb, language):
     """Console script for mlconjug."""
     conjugator = mlconjug.Conjugator(language)
