38e8ae65395ed6a6ba907f34a1c2345ae8a3c238
SekouD <sekou.diao@gmail.com>
2018-06-20 09:11:43 +0200

    Updated Type annotations.

--- mlconjug/mlconjug.py
+++ mlconjug/mlconjug.py
@@ -7,11 +7,21 @@ MLConjug Main module.
 
 | The module defines the classes needed to interface with Machine Learning models.
 
+
+| MIT License
+
+| Copyright (c) 2017, SekouD
+
+| Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+| The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+| THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 """
 
 # from mlconjug import _RESOURCE_PACKAGE, _LANGUAGE_FULL
-from .PyVerbiste import Verbiste, VerbInfo, Verb, VerbEn, VerbEs, VerbFr, VerbIt, \
-    VerbPt, VerbRo
+from .PyVerbiste import Verbiste, VerbInfo, Verb, VerbEn, VerbEs, VerbFr, VerbIt, VerbPt, VerbRo
 
 from sklearn.feature_selection import SelectFromModel
 from sklearn.feature_extraction.text import CountVectorizer
