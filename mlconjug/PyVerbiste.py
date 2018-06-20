f90fdf99a487f637c40e07c9a940ebd62d0a68ee
SekouD <sekou.diao@gmail.com>
2018-06-20 09:25:14 +0200

    Updated metadata.

--- mlconjug/PyVerbiste.py
+++ mlconjug/PyVerbiste.py
@@ -11,22 +11,15 @@ PyVerbiste.
 | The conjugation data conforms to the XML schema defined by Verbiste.
 | More information on Verbiste at https://perso.b2b2c.ca/~sarrazip/dev/verbiste.html
 
-| MIT License
-
-| Copyright (c) 2017, SekouD
-
-| Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-| The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-| THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
 """
 
 __author__ = 'SekouD'
 __author_email__ = 'sekoud.python@gmail.com'
-
+__copyright__ = "Copyright (c) 2017, SekouD"
+__credits__ = ["Pierre Sarrazin",]
+__license__ = "MIT"
+__maintainer__ = "SekouD"
+__status__ = "Production"
 
 import copy
 import xml.etree.ElementTree as ET
